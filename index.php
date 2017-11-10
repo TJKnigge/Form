3b3c90529ed4918b734a548d866d80a6cbf1981a
unknown <you@example.com>
2017-11-10 22:58:53 +0100

    ik moet uitzoeken wat here fout gaat

--- index.php
+++ index.php
@@ -17,34 +17,48 @@
     
     <body>
         
-        <form>
+        <form action="formfiller.php" method="post">
             <br>
-            First Name:<br> <input type="text" name="FirstName" value=""><br>
-            Surname:<br> <input type="text" name="Surname" value=""><br>
+            First Name:<br><input type="text" name="FirstName" value="" placeholder='First'><br>
+            Surname:<br> <input type="text" name="Surname" value="" placeholder='Surname'><br>
             Birth date:<br> <input type="date" name="bday"><br><br>
             Gender:<br><input type="radio" name="gender" value="male"> Male
-           
-            <input type="radio" name="gender" value="female"> Female<br><br><br><br>
+                       <input type="radio" name="gender" value="female"> Female<br><br><br><br>
             
             Address:<br><br>
             Street:<br><input type="text" name="Street" value="" required><br>
-            Zipcode & City:<br><input type="text" name="Zipcode" value=""> <input type="text" name="City" value="" required><br>
-            Country:<br><input list="Country" name="Country">
-                <datalist id="Country">
-                <option value="Netherlands">
-                <option value="Germany">
-                <option value="Belgium">
-                <option value="Great Britain">
-                <option value="France">
-                </datalist>
-              
+            Zipcode & City:<br><input type="text" name="Zipcode" value=""> <input type="text" name="City" value=""><br>
+            
+            Country:<br><select name="cnt" size="1">
+                        <option value="the Netherlands">the Netherlands</option>
+                        <option value="Germany">Germany</option>
+                        <option value="Belgium">Belgium</option>
+                        <option value="Great Britain">Great Britain</option>
+                        <option value="France">France</option>
+                        </select>
+            
+            
             
         </form>
         
-        <?php
+        <?php  //formfiller.php
+        
+        $form = fopen("TheForm.txt", "a+") or die("Unable to open file!");
+        
+        $list = <<<_END
         
         
-        // put your code here
+        $FirstName = get_post('FirstName');
+        $Surname = get_post('Surname');
+        
+       
+        _END
+        
+        fwrite($form, $list);
+       
+        
+        fclose($form);
+        
         ?>
         
     </body>
