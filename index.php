da14f10cc4f5b1983b7c28d7c65b2179bca6ab2e
unknown <you@example.com>
2017-11-10 15:16:24 +0100

    de start

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Forms </title>
+        
+        <script>
+        
+        </script>
+        
+        
+        <style>
+            
+            
+        </style>
+        
+    </head>
+    
+    <body>
+        
+        <form>
+            value:<br>
+            <input type="text" name="name" size="size" maxlength="length" value="value">
+            
+        </form>
+        
+        <?php
+        
+        
+        // put your code here
+        ?>
+        
+    </body>
+</html>
+
