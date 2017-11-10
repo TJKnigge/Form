29c73883ab2d63b3bc494217e2ba478ce7d0fa67
unknown <you@example.com>
2017-11-10 17:23:41 +0100

    first part complete

--- index.php
+++ index.php
@@ -23,7 +23,21 @@
             Surname:<br> <input type="text" name="Surname" value=""><br>
             Birth date:<br> <input type="date" name="bday"><br><br>
             Gender:<br><input type="radio" name="gender" value="male"> Male
-            <input type="radio" name="gender" value="female"> Female
+           
+            <input type="radio" name="gender" value="female"> Female<br><br><br><br>
+            
+            Address:<br><br>
+            Street:<br><input type="text" name="Street" value="" required><br>
+            Zipcode & City:<br><input type="text" name="Zipcode" value=""> <input type="text" name="City" value="" required><br>
+            Country:<br><input list="Country" name="Country">
+                <datalist id="Country">
+                <option value="Netherlands">
+                <option value="Germany">
+                <option value="Belgium">
+                <option value="Great Britain">
+                <option value="France">
+                </datalist>
+              
             
         </form>
         
