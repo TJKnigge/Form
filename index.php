c316a6a6f924224b7701aa228e473d7be68f3a52
unknown <you@example.com>
2017-11-11 19:12:24 +0100

    velden verplicht

--- index.php
+++ index.php
@@ -32,19 +32,20 @@
         <form method="post">
         
             <br>
-            First Name:<br><input type="text" name='FirstName' value="" maxlength="20"  size="10" placeholder='First'><br>
-            Surname:<br> <input type="text" name="Surname" value="" maxlength="20"  size="10" placeholder='Surname'><br>
-            Birth date:<br> <input type="date" name="bday"><br><br>
-            Gender:<br><input type="radio" name="gender" value="male"> Male
-                       <input type="radio" name="gender" value="female"> Female<br><br><br><br>
+            First Name:<br><input type="text" name='FirstName' value="" maxlength="20"  size="10" placeholder='First' required><br>
+            Surname:<br> <input type="text" name="Surname" value="" maxlength="20"  size="10" placeholder='Surname' required><br>
+            Birth date:<br> <input type="date" name="bday" required><br><br>
+            Gender:<br><input type="radio" name="gender" value="male" required> Male
+                       <input type="radio" name="gender" value="female" required> Female<br><br><br><br>
             
             Address:<br><br>
-            Street:<br><input type="text" name="Street" value="" maxlength="20"  size="10">
-            Housenumber<input type="text" name="num" value=""  maxlength="5"  size="4"><br>
-            Zipcode & City:<br><input type="text" name="Zipcode" value="" maxlength="20"  size="10"> 
-                               <input type="text" name="City" value="" maxlength="20"  size="10"><br>
+            Street:<br><input type="text" name="Street" value="" maxlength="20"  size="10" required>
+            Housenumber<input type="text" name="num" value=""  maxlength="5"  size="4" required><br>
+            Zipcode & City:<br><input type="text" name="Zipcode" value="" maxlength="20"  size="10" required> 
+                               <input type="text" name="City" value="" maxlength="20"  size="10" required><br>
             
-            Country:<br><select name="cnt" size="1" maxlength="20"  size="10">
+            Country:<br><select name="cnt" size="1" maxlength="20"  size="10" required>
+                        <option value=" "> </option>
                         <option value="the Netherlands">the Netherlands</option>
                         <option value="Germany">Germany</option>
                         <option value="Belgium">Belgium</option>
@@ -52,7 +53,7 @@
                         <option value="France">France</option>
                         </select>
             <br><br>
-            E-mail:<br> <input type="email" name="email"><br>
+            E-mail:<br> <input type="email" name="email" required><br>
             
             
             
